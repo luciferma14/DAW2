9a76a25713b9286c79ea39eeb7151269ff048c4e
Lucia Ferrandis Martinez <luciferma14@gmail.com>
2025-09-19 18:59:40 +0200

    Update

---
+++ DWEC/Scripts/script1.js
@@ -0,0 +1,23 @@
+
+
+
+let edad;
+let hijos;
+let sueldoNuevo;
+
+function procesar(){
+    let sueldoActual = document.getElementById("sActual");
+
+    let mensaje;
+        
+    mensaje = sueldoActual;
+
+    document.getElementById("resultado").textContent = mensaje;
+
+}
+
+
+// switch(sueldoActual){
+//     case edad < 30 && hijos == true: sueldoNuevo == 1200;
+
+// }
