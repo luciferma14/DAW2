0d75e79803acf6c9a7ffff8e0aa258eb5b1fa52a
Lucia <luciferma14@gmail.com>
2025-09-22 12:54:12 +0200

    Update

--- DWEC/Scripts/script1.js
+++ DWEC/Scripts/script1.js
@@ -1,23 +1,35 @@
 
-
-
+let sueldoActual;
 let edad;
 let hijos;
 let sueldoNuevo;
 
-function procesar(){
-    let sueldoActual = document.getElementById("sActual");
+function procesar(event){
+    event.preventDefault();  // Para que el script no recarge la página
 
-    let mensaje;
+    sueldoActual = document.getElementById("sActual").value;
+    edad = document.getElementById("edUsu").value;
+    hijos = document.getElementById("hijosUsu").value;
+
+
+    if(sueldoActual > 0 &&  edad < 30 && hijos == "si"){
         
-    mensaje = sueldoActual;
+        sueldoNuevo = 1200;
 
-    document.getElementById("resultado").textContent = mensaje;
+        document.getElementById("resultado").textContent = "Tu sueldo nuevo es: " + sueldoNuevo;
+    
+    }
+
+    document.getElementById("resultado").textContent = "Tu sueldo nuevo es: " + sueldoActual;
+
+
+    // switch(sueldoActual){
+    //     case edad < 30 && hijos == true: sueldoNuevo == 1200;
+
+    //     document.getElementById("resultado").textContent = "Tu sueldo actual es: " + sueldoNuevo;
+    
+    // }
 
 }
 
 
-// switch(sueldoActual){
-//     case edad < 30 && hijos == true: sueldoNuevo == 1200;
-
-// }
