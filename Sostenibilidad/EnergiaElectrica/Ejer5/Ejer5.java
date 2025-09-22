eb2df44bdaa73f715534bcd942b924175f3907fc
Lucia Ferrandis Martinez <luciferma14@gmail.com>
2025-09-22 15:09:54 +0200

    Update

--- Sostenibilidad/EnergiaElectrica/Ejer5/Ejer5.java
+++ Sostenibilidad/EnergiaElectrica/Ejer5/Ejer5.java
@@ -1,8 +1,23 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Ejer5{
+public class Ejer5{    
+
+    class Municipio{
+    
+        private String codigo; 
+        private String territorio;
+        private String valor;
+
+        public Municipio(String territorio, String valor, String codigo){
+            this.territorio = territorio;
+            this.valor = valor;
+            this.codigo = codigo;
+        } 
+    }
+
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
@@ -15,16 +30,23 @@ public class Ejer5{
             String codigo; 
             String territorio;
             String valor;
+            ArrayList <Municipio> lista = new ArrayList<>();
 
             try {
                 
                 FileReader fr = new FileReader(fichero);
-                 BufferedReader br = new BufferedReader(fr);
+                BufferedReader br = new BufferedReader(fr);
+                String linea1 = br.readLine();
 
                 while ((fichero = br.readLine()) != null){
                     codigo = fichero.split(";")[2];
                     territorio = fichero.split(";")[3];
                     valor = fichero.split(";")[4];
+
+                    try{
+                        lista.add(new Municipio(territorio, valor, codigo));
+
+                    }catch(Exception e){}
 
 
                     System.out.println("Codigo: " + codigo);
