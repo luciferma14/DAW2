0f662a92f6a7ea5fb1bba9097b8ad985badd4771
Lucia Ferrandis Martinez <luciferma14@gmail.com>
2025-10-06 19:56:19 +0200

    Update

--- DWES/EjerUP2/Ejer1.php
+++ DWES/EjerUP2/Ejer1.php
@@ -1,5 +1,5 @@
 <?php
-// Ejecutar: php -S localhost:8000 --> Navegador: http://localhost:8000/Ejer1.php
+// Ejecutar: php -S localhost:8000 --> Navegador: http://localhost:8000/DWES/EjerUP2/Ejer1.php
 function potencias($num, $exp){
 
     for($i = 1; $i <= $exp; $i++){
@@ -12,5 +12,7 @@ $resul = potencias(5, 4);
 
 foreach($resul as $i => $valor){
     echo "Potencia " . ($i+1) . " = $valor <br>";
+    $suma += $valor;
 }
+echo "Suma de las potencias: " . $suma;
 ?>
